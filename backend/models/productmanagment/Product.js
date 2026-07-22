b07f330bc10298d6348f64962e3307efedd63726
manalsoftech <manalsoftechmspl@gmail.com>
2026-07-22 14:40:50 +0530

    fix github actions ssh

--- backend/models/productmanagment/Product.js
+++ backend/models/productmanagment/Product.js
@@ -7,6 +7,7 @@ module.exports = (sequelize, DataTypes) => {
         autoIncrement: true,
         primaryKey: true
       },
+
       user_id: {
         type: DataTypes.INTEGER,
         allowNull: false
